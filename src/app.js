c763ac657bdcb298214fa8d3623f2e987efb0db1
Thiago Moreira <ttrmoreira@gmail.com>
2022-08-23 15:45:41 -0300

    Ajuste de mensagens

--- src/app.js
+++ src/app.js
@@ -2,6 +2,6 @@ const express = require("express");
 const app = express();
 
 app.get("/test", (_req, res) =>  {
-  res.status(200).send("Hello world")
+  res.status(200).send("Boas vindas ao universo Github Actions")
 })
 module.exports = app;
--- src/index.js
+++ src/index.js
@@ -2,5 +2,5 @@ const app =  require( "./app");
 const port = process.env.PORT || 3000;
 
 app.listen(port, () =>
-  console.log('Listening on port 3000!'),
+  console.log('Servidor na porta 3000!'),
 );

ee14985311f6f0de32e1c9cecdc0209e966dc230
Thiago Moreira <ttrmoreira@gmail.com>
2022-08-23 01:25:03 -0300

    teste

--- src/index.js
+++ src/index.js
@@ -2,5 +2,5 @@ const app =  require( "./app");
 const port = process.env.PORT || 3000;
 
 app.listen(port, () =>
-  console.log('Servidor de pé na porta 3000!'),
+  console.log('Listening on port 3000!'),
 );
